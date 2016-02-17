4dcdd9bb7fcf1f7b4ea9b5722d557dbb91687316
tcasey <trevort.casey@gmail.com>
2016-02-17 11:53:40 -0700

    Done

--- controller.js
+++ controller.js
@@ -1,12 +1,10 @@
 var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
-
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    mainService.getUsers().then(function(response) {
+      $scope.users = response.data.data;
+    });
   }
-
-
   $scope.getUsers();
-
 });
